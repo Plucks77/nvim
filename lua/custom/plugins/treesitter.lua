21b228d0085d2cd16ffe216cd358266488ee9671
Plucks77 <pedro.leoti.dev@gmail.com>
2026-06-11 11:31:35 -0300

    Fix Neovim 0.12 compatibility for treesitter and telescope.
    Migrate nvim-treesitter to main and telescope to master so preview and hover work with the new treesitter runtime.

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,18 +1,55 @@
+local ensure_installed = {
+  'bash',
+  'c',
+  'html',
+  'lua',
+  'markdown',
+  'ruby',
+  'vim',
+  'vimdoc',
+}
+
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
     build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
+    main = 'nvim-treesitter',
+    opts = {},
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local ft = vim.bo[args.buf].filetype
+          if ft == '' then
+            return
+          end
+
+          if not pcall(vim.treesitter.start, args.buf) then
+            return
+          end
+
+          if ft == 'ruby' then
+            vim.cmd.syntax 'enable'
+          elseif ft ~= 'ruby' then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
     config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
+      require('nvim-treesitter').setup(opts)
+
+      local already_installed = require('nvim-treesitter').get_installed 'parsers'
+      local to_install = vim.iter(ensure_installed)
+        :filter(function(lang)
+          return not vim.tbl_contains(already_installed, lang)
+        end)
+        :totable()
+
+      if #to_install > 0 then
+        require('nvim-treesitter').install(to_install)
+      end
     end,
   },
 }
